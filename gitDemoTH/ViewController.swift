c91b9a0e8700423ad773e0a5ab97344d4f1b980b
grayalas <alastair@wearealmond.com>
2016-09-06 15:53:32 +1200

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by Almond on 6/09/16.
+//  Copyright © 2016 Almond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
